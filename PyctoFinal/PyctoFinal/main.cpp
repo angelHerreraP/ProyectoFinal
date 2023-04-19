6f869dfefe047e337da633b8cb34d7cda3c46c39
Angel Perez <angelhp@Angels-MacBook-Air.local>
2023-04-19 11:36:40 -0600

    Inicio

---
+++ PyctoFinal/PyctoFinal/main.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+//Funcion de busqueda:
+
+
+// Juego 
+int main(int argc, const char * argv[]) {
+    // Variables
+    char numPlyrs;
+    
+
+    
+    
+    cout<<"Bienvenido"<<endl;
+    cout<<"**********************************"<<endl;
+    cout<<"Juguemos a Adivinemos el Numero, Se generara un numero al azar y el o los usuarios deberan encontrar el numero al azar."<<endl;
+    cout<<"Ingrese el numero de jugadores: ";
+    cout<<"a) Un Jugador"<<endl;
+    cout<<"b) Dos Jugadores"<<endl;
+    cout<<"c) Tres Jugadores"<<endl;
+    cin>>numPlyrs;
+    switch(numPlyrs){
+        case 'a':
+            break;
+        case 'b':
+            break;
+        case 'c':
+            break;
+    }
+    return 0;
+}
+
+
